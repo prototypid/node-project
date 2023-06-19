452d8c6651d54009d0d8b15f40a8f7993b97389e
Sahil Sharma <prototypid@gmail.com>
2023-06-19 20:11:45 +0530

    Add admin/shop routes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
+// routes
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 const app = express();
 
 // body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/admin", adminRoutes);
+
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.sendFile(path.join(__dirname, "./views/404.html"));
 });
 
 app.listen(3000);
