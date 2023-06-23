31fa32fc4880a0bc0fa50b2c46114ee3d581c7aa
Sahil Sharma <prototypid@gmail.com>
2023-06-23 09:03:21 +0530

    Models to interact with database

---
+++ models/product.js
@@ -0,0 +1,21 @@
+const db = require("../utils/database");
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {}
+
+  static deleteById(id) {}
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
+  }
+
+  static findById(id) {}
+};
