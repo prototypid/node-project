04c012661b7d5e924b2a1c8c76e9532f53700292
Sahil Sharma <prototypid@gmail.com>
2023-06-28 15:00:33 +0530

    Connect mongodb

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,31 @@
-const mysql = require("mysql2");
+const mongodb = require("mongodb");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-practice",
-  password: "1324",
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = pool.promise();
+let _db;
+
+const mongoConnect = (cb) => {
+  MongoClient.connect(
+    "mongodb://darq:1324@localhost:27017/?authMechanism=DEFAULT"
+  )
+    .then((client) => {
+      // client is a object which gives us access to the database.
+      console.log("Connected!");
+      _db = client.db("test");
+      cb();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
