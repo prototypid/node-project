420fb5a8af90b1518b7bb6247bef39bf7c9ba4f4
Sahil Sharma <prototypid@gmail.com>
2023-07-02 19:34:32 +0530

    Protect routes using middleware

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -10,14 +11,14 @@ router.get("/products", shopController.getProductsPage);
 
 router.get("/products/:productId", shopController.getProduct);
 
-router.get("/cart", shopController.renderCart);
+router.get("/cart", isAuth, shopController.renderCart);
 
-router.post("/cart", shopController.postCart);
+router.post("/cart", isAuth, shopController.postCart);
 
-router.post("/cart-delete-item", shopController.postDeleteCartProduct);
+router.post("/cart-delete-item", isAuth, shopController.postDeleteCartProduct);
 
-router.get("/orders", shopController.renderOrder);
+router.get("/orders", isAuth, shopController.renderOrder);
 
-router.post("/create-order", shopController.postOrder);
+router.post("/create-order", isAuth, shopController.postOrder);
 
 module.exports = router;
