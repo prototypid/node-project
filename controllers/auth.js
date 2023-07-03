19c1ebdeb1c5aea69dc760b887ba68c5b5dde461
Sahil Sharma <prototypid@gmail.com>
2023-07-03 11:30:33 +0530

    Add update password page to update password.

--- controllers/auth.js
+++ controllers/auth.js
@@ -173,3 +173,24 @@ exports.postReset = (req, res, next) => {
       .catch((err) => console.log(err));
   });
 };
+
+exports.getNewPassword = (req, res, next) => {
+  // retrieve token
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/password-reset", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+      });
+    })
+    .catch((err) => console.log(err));
+};
